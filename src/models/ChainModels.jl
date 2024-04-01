d45d95a07fc981ef275a27fd35f386eeee12f9d1
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2024-04-01 15:31:57 +0200

    more fix exports

--- src/models/ChainModels.jl
+++ src/models/ChainModels.jl
@@ -14,7 +14,7 @@ include("xxzmodel.jl")
 
 # from ising.jl
 export build_H_ising_manual,
-    build_H_ising_manual_lowtri, 
+    #build_H_ising_manual_lowtri, 
     build_H_ising, 
     build_H_ising_ZZ_X,
     build_H_ising_YY,
@@ -26,17 +26,17 @@ export build_H_ising_manual,
 
 #from potts.jl
 export build_H_potts_manual,
-    build_H_potts_manual_lowtri,
+    #build_H_potts_manual_lowtri,
     build_H_potts,
     build_expH_potts_2o,
     build_expH_potts_murg
 
 # from xxzmodel.jl
 export build_H_XXZ_manual,
-    build_H_XXZ_manual_lowtri,
+    #build_H_XXZ_manual_lowtri,
     build_H_XXZ,
-    build_expH_XXZ_1o,
-    build_expH_XXZ_2o,
+    #build_expH_XXZ_1o,
+    #build_expH_XXZ_2o,
     build_expH_XXZ_murg
 
 end
