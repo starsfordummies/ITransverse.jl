30227e8ebe01d4f65f291f71c42718d57ffa2d69
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2024-04-27 15:10:06 +0200

    init_arch probably not necessary really if we do startup.jl

--- src/init_arch.jl
+++
@@ -1,8 +0,0 @@
-if Base.Sys.islinux()
-    println("On linux - setting MKL and 8 threads")
-    using MKL
-    using LinearAlgebra
-
-    BLAS.set_num_threads(8)  # 8 threads seems to be a sweet spot
-    #unicodeplots()
-end
