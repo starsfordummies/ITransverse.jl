0fe7c5e5b41f31d146bffd99d88d8618b7941262
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2024-06-23 12:49:31 +0200

    typo ofc

--- src/ITenUtils/sqrt_itensor.jl
+++ src/ITenUtils/sqrt_itensor.jl
@@ -5,7 +5,7 @@ function Base.sqrt(a::ITensor)
      #y = adapt(mapreduce(NDTensors.unwrap_array_type, promote_type, x), random_mps(s))
 
     if isdiag(a, 1e-10) # make back to diagonal and sqrt it
-        sq_a = diag_itensor(sqrt.(array(diag(t1))), inds(t1))
+        sq_a = diag_itensor(sqrt.(array(diag(a))), inds(a))
     else
         a = NDTensors.cpu(a)
         sq_a = ITensor(sqrt(matrix(a)), inds(a))
