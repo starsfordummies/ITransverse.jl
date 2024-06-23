62e911ed037c99cdbfc4367c39bbacbc99ec7e48
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2024-06-23 12:47:04 +0200

    fixing gpu sqrt I hope

--- src/ITenUtils/sqrt_itensor.jl
+++ src/ITenUtils/sqrt_itensor.jl
@@ -4,8 +4,8 @@ function Base.sqrt(a::ITensor)
     data_type = promote_type(NDTensors.unwrap_array_type(a))
      #y = adapt(mapreduce(NDTensors.unwrap_array_type, promote_type, x), random_mps(s))
 
-    if isdiag(a, 1e-10)
-        sq_a = diagITensor(array(sqrt.(diag(a))), inds(a))
+    if isdiag(a, 1e-10) # make back to diagonal and sqrt it
+        sq_a = diag_itensor(sqrt.(array(diag(t1))), inds(t1))
     else
         a = NDTensors.cpu(a)
         sq_a = ITensor(sqrt(matrix(a)), inds(a))
