373285fc9e4941257c0c401259d3d96b6db4862b
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2024-06-24 10:14:48 +0200

    work on more flexible expvals_LR

--- src/lightcone/expvals_lr.jl
+++ src/lightcone/expvals_lr.jl
@@ -77,7 +77,54 @@ function expval_LR(ll::MPS, rr::MPS, opL::Vector{ComplexF64}, opR::Vector{Comple
 
 end
 
-""" TODO CHECK """
+
+""" Alternative way of computing expval_LR using open tMPOs and closing them. 
+We pass the list of local operators to compute as a (regular) MPO, 
+which we contract to the top of the tMPO 
+It may be more flexible """
+function expval_LR_open(ll::MPS, rr::MPS, ops::MPO, tp::tmpo_params)
+
+    # TODO allow for longer MPOs (longer lists of local ops)
+    
+    @assert length(ops) == 2
+
+    time_sites_L = siteinds(ll)
+    time_sites_R = siteinds(rr)
+
+    tMPO1, li1, ri1 = build_folded_open_tMPO(tp, time_sites_L)
+    tMPO2, li2, ri2 = build_folded_open_tMPO(tp, time_sites_R)
+
+    rho0 = (tp.init_state) * (tp.init_state')
+    tMPO1[end] *= ITensor(rho0, ri1)
+    tMPO2[end] *= ITensor(rho0, ri2)
+ 
+    ϵ_op[1] *= delta(combinedind(cs1), li1)
+    ϵ_op[2] *= delta(combinedind(cs2), li2)
+
+    LO = applys(tMPO1, ll)
+    OR = applys(tMPO2, rr) # todo swap indices for non-symmetric MPOs
+
+    insert!(LO.data, 1, ϵ_op[1])
+    insert!(OR.data, 1, ϵ_op[2])
+
+    ev_LOOR = overlap_noconj(LO, OR)
+
+    deleteat!(LO.data,1)
+    deleteat!(OR.data,1)
+
+    LO[1] *= ITensor(ComplexF64[1,0,0,1], li1)
+    OR[1] *= ITensor(ComplexF64[1,0,0,1], li2)
+
+    #normalization 
+    ev_L11R = overlap_noconj(LO, OR)
+
+    return ev_LOOR/ev_L11R
+
+end
+
+
+
+""" Compute exp value for energy density in Ising """
 function expval_en_density(ll::MPS, rr::MPS, tp::tmpo_params)
 
     time_sites_L = siteinds(ll)
@@ -90,22 +137,9 @@ function expval_en_density(ll::MPS, rr::MPS, tp::tmpo_params)
     tMPO1[end] *= ITensor(rho0, ri1)
     tMPO2[end] *= ITensor(rho0, ri2)
 
-    temp_s = siteinds("S=1/2",2)
-    os = OpSum()
-    os += tp.mp.JXX,   "X",1,"X",2
-    os += tp.mp.hz/2,  "I",1,"Z",2
-    os += tp.mp.hz/2,  "Z",1,"I",2
-    os += tp.mp.λx/2,  "I",1,"X",2
-    os += tp.mp.λx/2,  "X",1,"I",2
-
-    #ϵ_op = ITensor(os, temp_s, temp_s')
-    ϵ_op = MPO(os, temp_s)
-    cs1 = combiner(temp_s[1], temp_s[1]')
-    cs2 = combiner(temp_s[2], temp_s[2]')
-    ϵ_op[1] *= cs1 
-    ϵ_op[2] *= cs2 
-    ϵ_op[1] *= delta(combinedind(cs1), li1)
-    ϵ_op[2] *= delta(combinedind(cs2), li2)
+    ϵ_op = ITransverse.ChainModels.epsilon_brick_ising(tp)
+    ϵ_op[1] *= delta(siteind(ϵ_op,1), li1)
+    ϵ_op[2] *= delta(siteind(ϵ_op,2), li2)
 
     LO = applys(tMPO1, ll)
     OR = applys(tMPO2, rr) # todo swap indices for non-symmetric MPOs
@@ -205,6 +239,8 @@ function compute_expvals(ll::AbstractMPS, rr::AbstractMPS, op_list::Vector{Strin
         elseif op == "eps"
             #println("eps")
             allevs["eps"] = expval_en_density(ll, rr, tp)
+        else
+            @warn "$(op) not implemented"
         end
     end
 
