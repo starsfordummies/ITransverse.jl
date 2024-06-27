32249c70dc4f3c9e1376bd43970ef5f43ee632d9
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2024-06-28 00:30:39 +0200

    [break] new tmpo_params and pm_params

--- src/ITenUtils/pparams.jl
+++ src/ITenUtils/pparams.jl
@@ -1,15 +1,6 @@
 # Custom param structures for Ising-Potts models
 # and for power methods/ truncations etc 
 
-""" This is old and should be superseded soon"""
-struct pparams
-    JXX::Float64
-    hz::Float64
-    dt::Float64
-    nbeta::Int64
-    init_state::Vector{ComplexF64}
-end
-
 struct model_params
     phys_space::String
     JXX::Float64
@@ -26,13 +17,10 @@ model_params(x::model_params;
     dt=x.dt) = model_params(x.phys_space, JXX, hz, λx, dt)
 
 struct tmpo_params
-    time_phys_space::String
-    virt_space::String
     expH_func::Function
     mp::model_params
     nbeta::Int64
     init_state::Vector{ComplexF64}
-   
 end
 
  # allow for changes on the fly of params
@@ -40,33 +28,20 @@ tmpo_params(x::tmpo_params;
     expH_func=x.expH_func, 
     mp=x.model_params,
     nbeta=x.model,
-    init_state=x.init_state) = tmpo_params(x.time_phys_space, x.virt_space, expH_func, mp, nbeta, init_state)
+    init_state=x.init_state) = tmpo_params(expH_func, mp, nbeta, init_state)
 
 
-struct ppm_params
+struct pm_params
     itermax::Int64
     cutoff::Float64
     maxbondim::Int64
-    verbose::Bool
     ds2_converged::Float64
     increase_chi::Bool
-    plot_s::Bool
     ortho_method::String
 
-    function ppm_params(; itermax::Int64 = 400, cutoff::Float64=1e-12, maxbondim::Int64=100, 
-        verbose::Bool=false, ds2_converged::Float64=1e-5, increase_chi::Bool=false, plot_s::Bool=false, ortho_method::String="SVD")
-        return new(itermax, cutoff, maxbondim, verbose, ds2_converged, increase_chi, plot_s, ortho_method)
-    end
-
-
-    # past-proof functions to be erased in the future
-    function ppm_params(itermax::Int64,
-        cutoff::Float64,
-        maxbondim::Int64,
-        verbose::Bool,
-        ds2_converged::Float64,
-        increase_chi::Bool=true)
-        return new(itermax,cutoff,maxbondim,verbose,ds2_converged, increase_chi, false, "SVD")
+    function pm_params(; itermax::Int64 = 400, cutoff::Float64=1e-12, maxbondim::Int64=100, 
+        ds2_converged::Float64=1e-5, increase_chi::Bool=false, ortho_method::String="SVD")
+        return new(itermax, cutoff, maxbondim, ds2_converged, increase_chi, ortho_method)
     end
 
 end
