b19cdea95373406bf47ad7e1b9e4c8f9c1235324
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2024-07-02 10:53:03 +0200

    added test for light cone builder and tensors

---
+++ test/test_build_cone.jl
@@ -0,0 +1,19 @@
+using ITensors, ITensorMPS
+using Revise
+using Test
+
+using ITransverse
+using ITransverse: init_cone_n
+
+@testset "Light cone initialization and tensors for Ising" begin
+tp = ising_tp()
+
+evs_X = ComplexF64[]
+for nn = 1:6
+    c0 = init_cone_n(tp,nn)
+    push!(evs_X, expval_LR(c0,c0,[0,1,1,0], tp))
+end
+
+@test norm(evs_X - ITransverse.ITenUtils.bench_X_04_plus[1:6]) < 1e-12
+end
+
