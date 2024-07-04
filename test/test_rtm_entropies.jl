36809ed1295ea52a3a8bdbbbcdd06da6a74f80c8
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2024-07-04 12:30:00 +0200

    tests for rtm entropies

--- test/test_rtm_entropies.jl
+++ test/test_rtm_entropies.jl
@@ -42,3 +42,51 @@ r2_cut = rtm2_contracted(ll, ll, 10)
 @test abs(inner(ll,llc) - 1) < 1e-12
 
 end
+
+
+@testset "Testing right gauge symmetric RTM diagonalization vs full diag" begin
+
+    psi = quick_mps(20, 30)
+    
+    mpslen = length(psi)
+    
+    psi_gauged = gen_canonical_right(psi)
+    
+    lenv= ITensor(1.)
+    s = siteinds(psi_gauged)
+    
+    eigs_rho = []
+    eigs_rho_check = []
+    
+    for jj in 1:mpslen-1
+    
+        lenv *= psi_gauged[jj]
+        lenv *= psi_gauged[jj]'
+        lenv *= delta(s[jj],s[jj]' )
+    
+        @assert ndims(lenv) == 2 
+    
+        vals, vecs = eigen(lenv, ind(lenv,1), ind(lenv,2))
+    
+        push!(eigs_rho, vals)
+    
+        if mpslen - jj < 5
+            renv = ITensor(1.)
+            for kk in mpslen:-1:jj+1
+                renv *= psi_gauged[kk]
+                renv *= psi_gauged[kk]'
+            end
+    
+            rtm_full = lenv * renv
+            #@info "RTM full size: $(size(rtm_full))"
+            vals_full, _ = eigen(rtm_full, inds(rtm_full,plev=0), inds(rtm_full,plev=1))
+    
+            push!(eigs_rho_check, vals_full)
+        end
+    
+    end
+    
+    
+    @test diag.(matrix.((eigs_rho[end-3:end]))) ≈ diag.(matrix.(eigs_rho_check))
+    
+    end
