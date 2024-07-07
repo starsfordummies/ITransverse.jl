9800dc2c3b9234d05ca07c0e4aacf6e847a3c56e
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2024-07-07 20:28:34 +0200

    begin reworking gpu

--- ext/ITransverseGPUExt/ITransverseGPUExt.jl
+++ ext/ITransverseGPUExt/ITransverseGPUExt.jl
@@ -2,25 +2,5 @@ module ITransverseGPUExt
 
 using CUDA
 
-using ITensors
-using ITensorMPS
-using ITransverse 
-using ProgressMeter
-using JLD2
 
-
-include("gpu_expvals.jl")
-include("gpu_sweeps.jl")
-include("gpu_cone.jl")
-include("gpu_cone_svd.jl")
-
-export gpu_run_cone, 
-gpu_run_cone_svd,
-gpu_truncate_sweep,
-gpu_truncate_sweep!,
-gpu_expval_LR,
-cpu_expval_LR,
-gpu_expval_LL_sym,
-gpu_compute_expvals
-    
 end #module ITransverseGPUExt
