a5b5e6d95f70fbf6ccd6df66e903592d6954bcbd
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2024-07-08 11:24:21 +0200

    rework GPUext jl

--- ext/ITransverseGPUExt/ITransverseGPUExt.jl
+++ ext/ITransverseGPUExt/ITransverseGPUExt.jl
@@ -9,17 +9,22 @@ using ProgressMeter
 using JLD2
 
 
-include("gpu_expvals.jl")
-include("gpu_sweeps.jl")
-include("gpu_cone.jl")
-include("gpu_cone_svd.jl")
+# include("gpu_expvals.jl")
+# include("gpu_sweeps.jl")
+# include("gpu_cone.jl")
+# include("gpu_cone_svd.jl")
 
 
 device(x) = NDTensors.cu(x)
 
-function device(b::FoldtMPOBlocks)
-    return FoldtMPOBlocks(device(b.WWl), device(b.WWc), device(b.WWr), device(b.rho0), tp, inds_ww)
-end
+# function device(b::Struct)
+#     cuvals= NDTensors.cu.(getfield.((b,), fieldnames(typeof(b))))
+#     return typeof(b)(cuvals)
+# end
+
+# function device(b::FoldtMPOBlocks)
+#     return FoldtMPOBlocks(device(b.WWl), device(b.WWc), device(b.WWr), device(b.rho0), tp, inds_ww)
+# end
 
     
 end #module ITransverseGPUExt
