5e6f8af952a8d3acf2a365def1dabe2675ee092f
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2024-07-12 09:31:03 +0200

    removing ortho_method from TruncParams

--- src/sweeps/trunc_params.jl
+++ src/sweeps/trunc_params.jl
@@ -1,12 +1,10 @@
 struct TruncParams
     cutoff::Float64
     maxbondim::Int64
-    ortho_method::String
-
 end
 
 # just some default for when we're lazy
-TruncParams() = TruncParams(1e-10, 100, "SVD")
+TruncParams() = TruncParams(1e-10, 100)
 
  
 
