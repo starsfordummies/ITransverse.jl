b5c3590caeb2568bcf1d8cb55929ff929273315b
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2024-07-14 15:49:38 +0200

    typo

--- src/tmpo/tmpo_blocks.jl
+++ src/tmpo/tmpo_blocks.jl
@@ -65,8 +65,7 @@ struct FwtMPOBlocks
     rot_inds::Dict
 
   
-    function FwtMPOBlocks(Wl::ITensor,Wc::ITensor,Wr::ITensor,
-        tp::ITensor,rot_inds::Dict) 
+    function FwtMPOBlocks(Wl::ITensor,Wc::ITensor,Wr::ITensor, tp::tmpo_params,rot_inds::Dict) 
 
         # The data type of the bottom-left term in tp dictates whether the *full* thing will lie on GPU
         dttype = NDTensors.unwrap_array_type(tp.bl)
