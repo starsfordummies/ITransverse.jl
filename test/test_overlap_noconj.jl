a4e92a35331230d3e2e785ea3d469458fa6cb8b0
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2024-11-05 12:11:15 +0100

    fix test

--- test/test_overlap_noconj.jl
+++ test/test_overlap_noconj.jl
@@ -4,7 +4,7 @@ using Test
 using ITransverse
 
 ss = siteinds(4, 20)
-psi = random_mps(ss, linkdims=30)
-phi = random_mps(ss, linkdims= 20)
+psi = random_mps(ComplexF64, ss, linkdims=30)
+phi = random_mps(ComplexF64, ss, linkdims= 20)
 
 @test overlap_noconj(psi, phi) ≈ ITransverse.overlap_noconj_ite(psi,phi)
