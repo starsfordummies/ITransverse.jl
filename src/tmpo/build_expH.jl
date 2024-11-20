664a426f9976a8a1e2d3d51cd1f647a83af2378b
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2024-11-20 19:05:43 +0100

    quick aliases for playing around

--- src/tmpo/build_expH.jl
+++ src/tmpo/build_expH.jl
@@ -8,3 +8,10 @@ function build_expHim(tp::tMPOParams)
     tp.expH_func(tp.mp, dt=-im*tp.dt )
 end
 
+""" Quick building a FoldtMPOBlocks struct for playing around """
+function quick_b()
+
+    tp = ising_tp()
+    b = FoldtMPOBlocks(tp)
+    return b
+end
