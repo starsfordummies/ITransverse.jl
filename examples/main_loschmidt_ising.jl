7b23ba454291a0b60a2c22d0f4e1366158ad113c
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2024-11-28 15:16:42 +0100

    compute dominant values in Loschmidt

--- examples/main_loschmidt_ising.jl
+++ examples/main_loschmidt_ising.jl
@@ -5,7 +5,7 @@ using ITransverse
 function main_ising_loschmidt(Tstart::Int, Tend::Int, nbeta::Int; Tstep::Int=1)
 
     JXX = 1.0
-    hz = 1.0
+    hz = 0.9
     gx = 0.0
 
     dt = 0.1
@@ -13,25 +13,27 @@ function main_ising_loschmidt(Tstart::Int, Tend::Int, nbeta::Int; Tstep::Int=1)
     zero_state = Vector{ComplexF64}([1, 0])
     plus_state = Vector{ComplexF64}([1 / sqrt(2), 1 / sqrt(2)])
 
-    #init_state = plus_state
-    init_state = zero_state
+    init_state = plus_state
+    #init_state = zero_state
 
 
-    cutoff = 1e-16
+    cutoff = 1e-12
     maxbondim = 140
     itermax = 800
     eps_converged = 1e-6
 
     truncp = TruncParams(cutoff, maxbondim)
 
-    pm_params = PMParams(truncp, itermax, eps_converged, true, "RTM")
+    pm_params = PMParams(truncp, itermax, eps_converged, true, "RDM")
 
     mp = ModelParams("S=1/2", JXX, hz, gx)
     tp = tMPOParams(dt, build_expH_ising_murg, mp, nbeta, init_state, init_state)
 
     ll_murgs = Vector{MPS}()
-
     ds2s = Vector{Float64}[]
+    leading_eigs = ComplexF64[]
+    leading_eigsq = ComplexF64[]
+    overlapsLR = ComplexF64[]
 
     Ntime_steps = Tstart
     Nsteps = Ntime_steps + 2 * nbeta
@@ -63,24 +65,36 @@ function main_ising_loschmidt(Tstart::Int, Tend::Int, nbeta::Int; Tstep::Int=1)
 
         psi_trunc, ds2 = powermethod_sym(start_mps, mpo, pm_params)
 
-        # @show inner(psi_trunc, psi_trunc2)/norm(psi_trunc)^2
+
+        leading_eig = inner(conj(psi_trunc'), mpo, psi_trunc)
+
+        # silly extra check so we can see that (LTTR) = lambda^2 (LR)
+        OL = apply(mpo, psi_trunc,  alg="naive", truncate=false)
+        leading_sq = overlap_noconj(OL, OL)
+
+        normalization = overlap_noconj(psi_trunc,psi_trunc)
+        leading_eig, leading_sq, normalization
 
         push!(ll_murgs, psi_trunc)
         push!(ds2s, ds2)
+        push!(leading_eigs, leading_eig)
+        push!(leading_eigsq, leading_sq)
+        push!(overlapsLR, normalization)
 
         curr_T = ts
 
 
         if ts % 20 == 0
             out_filename = "cp_ising_$(ts)_$(maxlinkdim(psi_trunc)).jld2"
-            jldsave(out_filename; ll_murgs, ds2s, tp, pm_params, curr_T, allts)
+            jldsave(out_filename; ll_murgs, ds2s, tp, pm_params, curr_T, allts, leading_eigs, leading_eigsq, overlapsLR)
         end
 
     end
 
-    return ll_murgs, ds2s
+    return ll_murgs, ds2s, leading_eigs, leading_eigsq, overlapsLR
 
 
 end
 
-psis, ds2s = main_ising_loschmidt(50, 50, 0)
+
+psis, ds2s, leading_eigs, leading_eigsq, overlapsLR = main_ising_loschmidt(10, 120, 2; Tstep=4)
