705c5cf4cab48fbfe3dd76cc454991b80893b711
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2024-12-10 17:30:21 +0100

    add fwtMPOn_initbetaonly where we only put imag steps on one side

--- src/tmpo/build_tmpo_fw.jl
+++ src/tmpo/build_tmpo_fw.jl
@@ -171,3 +171,65 @@ function fw_tMPOn(b::FwtMPOBlocks, b_im::FwtMPOBlocks, time_sites::Vector{<:Inde
     return tMPO, tMPS
 
 end
+
+
+
+function fw_tMPOn_initbetaonly(b::FwtMPOBlocks, b_im::FwtMPOBlocks, time_sites::Vector{<:Index}; 
+    left_state::ITensor = b.tp.bl, right_state::ITensor = b.tp.tr)
+
+    tp = b.tp
+
+    nbeta = tp.nbeta*2
+    #left_state = tp.bl
+    #right_state = tp.tr 
+
+    @assert nbeta < length(time_sites) - 2
+
+    Nsteps = length(time_sites)
+
+    # Rotated indices already 
+    Wc = b.Wc
+    (icL, icR, icP, icPs) = inds(Wc)
+    Wc_im = b_im.Wc
+
+    Wr = b.Wr
+    (irL, irR, irP) = inds(Wr)
+
+    Wr_im = b_im.Wr
+
+    # Make same indices for real and imag, it's easier aftwards 
+    replaceinds!(Wc_im, inds(Wc_im), inds(Wc))
+    replaceinds!(Wr_im, inds(Wr_im), inds(Wr))
+
+    rot_links_mpo = [Index(dim(icL), "Link,rotl=$ii") for ii in 1:(Nsteps - 1)]
+    rot_links_mps = sim(rot_links_mpo)
+
+
+    tMPO =  MPO(fill(Wc, Nsteps))
+    tMPS =  MPS(fill(Wr, Nsteps))
+
+    for ii = 1:nbeta
+        tMPO[ii] = (Wc_im) * delta(icP, time_sites[ii]) * delta(icPs, time_sites[ii]')
+        tMPS[ii] = (Wr_im) * delta(irP, time_sites[ii]) 
+    end
+    for ii = nbeta+1:Nsteps
+        tMPO[ii] = tMPO[ii] * delta(icP, time_sites[ii]) * delta(icPs, time_sites[ii]') 
+        tMPS[ii] = tMPS[ii] * delta(irP, time_sites[ii])
+    end
+
+
+    # Contract edges with boundary states, label linkinds
+    tMPO[1] = tMPO[1] * left_state * delta(ind(left_state,1), icL) * delta(icR, rot_links_mpo[1]) 
+    tMPS[1] = tMPS[1] * left_state * delta(ind(left_state,1), irL) * delta(irR, rot_links_mps[1]) 
+
+    for ii = 2:Nsteps-1
+        tMPO[ii] = tMPO[ii] * delta(icL, rot_links_mpo[ii-1]) * delta(icR, rot_links_mpo[ii]) 
+        tMPS[ii] = tMPS[ii] * delta(irL, rot_links_mps[ii-1]) * delta(irR, rot_links_mps[ii]) 
+    end
+
+    tMPO[end] = tMPO[end] * delta(icL, rot_links_mpo[Nsteps-1]) * dag(right_state) * delta(ind(right_state,1),icR) 
+    tMPS[end] = tMPS[end] * delta(irL, rot_links_mps[Nsteps-1]) * dag(right_state) * delta(ind(right_state,1),irR) 
+
+    return tMPO, tMPS
+
+end
