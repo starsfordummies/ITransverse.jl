65bd3e0da1d54be2f5c7132565302b0ad07c29c0
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2025-02-08 15:07:56 +0100

    change defaults for TruncParams()

--- src/sweeps/trunc_params.jl
+++ src/sweeps/trunc_params.jl
@@ -5,7 +5,7 @@ struct TruncParams
 end
 
 # just some default for when we're lazy
-TruncParams() = TruncParams(1e-10, 100)
+TruncParams() = TruncParams(1e-12, 256)
 
 # Default to right sweeps
 TruncParams(cutoff::Float64, maxbondim::Int64) = TruncParams(cutoff, maxbondim, "right")
