357d424d6d4d7f04eed0a21ae3c621f11bad2242
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2025-05-09 20:32:01 +0200

    sane indices in Loschmidt as well

--- src/tmpo/build_tmpo_fw.jl
+++ src/tmpo/build_tmpo_fw.jl
@@ -201,11 +201,11 @@ function fw_tMPOn_initbetaonly(b::FwtMPOBlocks, b_im::FwtMPOBlocks, time_sites::
 
     # Rotated indices already 
     Wc = b.Wc
-    (icL, icR, icP, icPs) = inds(Wc)
+    (icL, icR, icP, icPs) = (b.rot_inds[:L], b.rot_inds[:R], b.rot_inds[:P], b.rot_inds[:Ps]) 
     Wc_im = b_im.Wc
 
     Wr = b.Wr
-    (irL, irR, irP) = inds(Wr)
+    (irL, irR, irP) = (b.rot_inds[:L], b.rot_inds[:R], b.rot_inds[:P]) 
 
     Wr_im = b_im.Wr
 
