7da666863cd0c23d69a6996cc087c70ad4c38d54
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2025-05-13 19:34:30 +0200

    allow to build FoldtMPOBlocks from spatial MPO of U(t)

--- src/tmpo/fold_tmpo_blocks.jl
+++ src/tmpo/fold_tmpo_blocks.jl
@@ -40,23 +40,7 @@ function FoldtMPOBlocks(tp::tMPOParams, init_state::ITensor = tp.bl)
     WWl = replaceinds(WWl, (L,R,P,Ps),(time_P',time_P,time_L, time_R))
     WWc = replaceinds(WWc, (L,R,P,Ps),(time_P',time_P,time_L, time_R))
     WWr = replaceinds(WWr, (L,R,P,Ps),(time_P',time_P,time_L, time_R))
-    # iwl = (Index(1), inds(WWl)...) 
-    # WWl = replaceinds(WWl, iwl, inds(WWc))
-    # iwr = (ind(WWr,1), Index(1), ind(WWr,2),ind(WWr,3))
-    # WWr = replaceinds(WWr, iwr, inds(WWc))
-
-    # WWl, _ = rotate_90clockwise(WWl; R,P,Ps)
-    # WWc, rotated_inds= rotate_90clockwise(WWc; L,R,P,Ps)
-    # WWr, _ = rotate_90clockwise(WWr; L,P,Ps)
-
-    # # Match the inds of all three tensors FIXME do we want this? 
-    # # TODO check: rotated ints should be L, R, P, P' 
-    # iwl = (Index(1), inds(WWl)...) 
-    # WWl = replaceinds(WWl, iwl, inds(WWc))
-    # iwr = (ind(WWr,1), Index(1), ind(WWr,2),ind(WWr,3))
-    # WWr = replaceinds(WWr, iwr, inds(WWc))
-
-
+   
     # Handling of initial state 
     # We can accept either an initial state or initial folded state (DM)
     iP = phys_ind(init_state)
@@ -106,3 +90,59 @@ function FoldtMPOBlocks(b::FoldtMPOBlocks;
     return FoldtMPOBlocks(WWl, WWc, WWr, rho0, tp, rot_inds)
 end
 
+""" Allow to pass a spatial MPO for U(t) and other tMPO params as input and build everything else from that """ 
+function FoldtMPOBlocks(dt::Number, eH_space_mpo::MPO, init_state::ITensor, top_op::ITensor, nbeta::Int=0) 
+
+    WWl, WWc, WWr,  (L, R, P, Ps) = build_WW(eH_space_mpo)
+
+    time_P = Index(dim(L), "Site,time")
+    time_L = Index(dim(P), "Link,time")
+    time_R = Index(dim(Ps), "Link,time")
+   
+    WWl = replaceinds(WWl, (L,R,P,Ps),(time_P',time_P,time_L, time_R))
+    WWc = replaceinds(WWc, (L,R,P,Ps),(time_P',time_P,time_L, time_R))
+    WWr = replaceinds(WWr, (L,R,P,Ps),(time_P',time_P,time_L, time_R))
+   
+    # Handling of initial state 
+    # We can accept either an initial state or initial folded state (DM)
+    iP = phys_ind(init_state)
+    physdim_init = dim(iP)
+
+    @assert ndims(init_state) == 1 || ndims(init_state) == 3
+
+    lrinds = uniqueinds(inds(init_state), iP)
+    if !isempty(lrinds)
+        @assert dim(lrinds[1]) == dim(lrinds[2])  # translational invariance 
+    end
+
+    # Check if we need to fold 
+    if physdim_init == dim(P)
+        rho0 = init_state * delta(iP, Index(dim(P),"Link,time,rho0"))
+        if !isempty(lrinds)
+            iP_rho0 = Index(dim(rho0,1),"rho0,time,Site")
+            rho0 = replaceinds(rho0, lrinds, (iP_rho0, iP_rho0'))
+        end
+    elseif physdim_init == dim(P) ÷ 2
+        rho0 = (init_state) * (init_state') 
+        comb_phys = combiner(iP, iP')
+        rho0 *= comb_phys
+        
+        if !isempty(lrinds)
+            ciileft = combiner(lrinds[1], lrinds[1]') 
+            rho0 *= ciileft
+            ciiright = combiner(lrinds[2], lrinds[2]') 
+            rho0 *= ciiright
+            iP_rho0 = Index(dim(combinedind(ciileft)),"rho0,time,Site")
+            #@show iP_rho0
+            #@show ciileft
+            rho0 = replaceinds(rho0, (combinedind(ciileft), combinedind(ciiright)), (iP_rho0, iP_rho0'))
+        end
+        rho0 *= delta(combinedind(comb_phys), Index(dim(P), "Link,time,rho0"))
+    else
+        @error "Dimension of init_state is $(physdim_init) vs linkdim $(dim(P))"
+    end
+
+    tp = tMPOParams(dt, nothing, nothing, nbeta, init_state, top_op)
+    inds_ww = Dict(:Ps => time_P',:P => time_P, :L => time_L, :R=> time_R) # TODO
+    return FoldtMPOBlocks(WWl, WWc, WWr, rho0, tp, inds_ww)
+end
