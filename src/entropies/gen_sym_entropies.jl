09ae4990f1e68eebd423bd63a4c7fb27a810d9e3
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2025-06-27 16:58:27 +0200

    return generalized vn symmetric entropies in correct order, add generalized_vn_entropy_symmetric(psi, cut)

--- src/entropies/gen_sym_entropies.jl
+++ src/entropies/gen_sym_entropies.jl
@@ -10,20 +10,19 @@ function generalized_vn_entropy_symmetric(psiL::MPS; bring_left_gen::Bool=true, 
     end
 
     mpslen = length(psiL)
-    #links = linkinds(psiL)
 
     overlap = overlap_noconj(psiL,psiL)
     if abs(1-overlap) > 1e-4 && !normalize_eigs
         @warn" overlap not 1: $(overlap)"
     end
     
-    gen_ents = ComplexF64[]
+    gen_ents = Vector{ComplexF64}(undef, mpslen-1) 
 
     right_env = ITensor(1.)
 
     psiR = prime(linkinds, psiL)
 
-    # Start from the operator/final state side (for me that's on the left)
+    # Start from the *right* (operator side)
     for ii = mpslen:-1:2
         Ai = psiL[ii]
         #Bi = prime(Ai, "v") # consistent with the label assigned by generalized canon form
@@ -54,13 +53,63 @@ function generalized_vn_entropy_symmetric(psiL::MPS; bring_left_gen::Bool=true, 
             #println("[$ii]temp = $(gen_ent_cut) | sum = $(sum(eigss))")
         end
 
-        push!(gen_ents, gen_ent_cut)
+        gen_ents[ii-1] = gen_ent_cut
     
     end
 
     return gen_ents
     
 end
+function generalized_vn_entropy_symmetric(psiL::MPS, cut::Int; bring_left_gen::Bool=true, normalize_eigs::Bool=true)
+ 
+    if bring_left_gen
+        psiL = gen_canonical_left(psiL)
+    end
+
+    mpslen = length(psiL)
+    #links = linkinds(psiL)
+
+    overlap = overlap_noconj(psiL,psiL)
+    if abs(1-overlap) > 1e-4 && !normalize_eigs
+        @warn" overlap not 1: $(overlap)"
+    end
+    
+    psiR = prime(linkinds, psiL)
+
+
+    # Build right_env up to 'cut'
+    right_env = ITensor(1.)
+
+    # Start from the *right* (operator side)
+    for ii = mpslen:-1:cut
+        right_env = psiL[ii] * right_env 
+        right_env = psiR[ii] * right_env
+    end
+
+    @assert order(right_env) == 2 
+    #println(left_env)
+    eigss, _ = eigen(right_env, inds(right_env)[1],inds(right_env)[2])
+    #gen_ent_cut = sum(eigss.*log.(eigss))
+    
+    if normalize_eigs
+        eigss = eigss/sum(eigss) 
+    else # If we don't normalize, warn if normalization is off
+        if abs(sum(eigss) - 1.) > 0.01
+            @warn "RTM not well normalized? Σeigs = 1-$(abs(sum(eigss) - 1.)) "
+        end
+    end
+
+    gen_ent_cut = ComplexF64(0.)
+    for n=1:dim(eigss, 1)
+        p = eigss[n,n]  
+        gen_ent_cut -= p * log(p)
+    end
+
+    return gen_ent_cut
+    
+end
+
+
 
 """
 Generalized entropy for a *symmetric* environment (psiL,psiL)
