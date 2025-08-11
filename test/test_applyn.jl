32cb468343c038363116d7e99f7e2d9b93281585
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2025-08-11 15:40:16 +0200

    better test applyn()

--- test/test_applyn.jl
+++ test/test_applyn.jl
@@ -2,26 +2,49 @@ using ITensors, ITensorMPS
 using Test
 
 using ITransverse
+using ITransverse.ITenUtils: check_mps_sanity
 
 b = FoldtMPOBlocks(ising_tp())
 ss = siteinds(4, 10)
-psi = random_mps(ss[1:end-3], linkdims=20)
-ooo = ITransverse.folded_tMPO_ext(b, ss; LR="R", n_ext=3, fold_op=[1,0,0,-1])
 
-psi_ext = applyn(ooo,psi)
 
-siteinds(psi_ext) == ss
+n_ext = 3 
 
-phi = random_mps(ss[1:end-2], linkdims=20)
+oooR = ITransverse.folded_tMPO_ext(b, ss; LR="R", n_ext, fold_op=[1,0,0,-1])
+@test count(isempty, siteinds(oooR,plev=0)) == n_ext
+@test count(isempty, siteinds(oooR,plev=1)) == 0
 
-ooo = ITransverse.folded_tMPO_ext(b, ss; LR="L", n_ext=2, fold_op=[1,0,0,-1])
+oooL = ITransverse.folded_tMPO_ext(b, ss; LR="L", n_ext, fold_op=[1,0,0,-1])
+@test count(isempty, siteinds(oooL,plev=1)) == n_ext
+@test count(isempty, siteinds(oooL,plev=0)) == 0
 
-phi_ext = applyns(ooo,phi)
 
-siteinds(phi_ext) == ss
+# Apply Right MPO to (shorter) Right tMPS: Extend 
+psiR = random_mps(ss[1:end-n_ext], linkdims=20)
 
-ooo = ITransverse.folded_tMPO_ext(b, ss; LR="L", n_ext=4, fold_op=[1,0,0,-1])
+psiR_ext = applyn(oooR,psiR)
+@test siteinds(psiR_ext) == ss
+@test check_mps_sanity(psiR_ext)
 
-rho = random_mps(ss, linkdims=20)
 
-rho_cut = applyn(ooo,rho)
+# Apply Left tMPO to (shorter) Left tMPS: Extend
+psiL = random_mps(ss[1:end-n_ext], linkdims=20)
+psiL_ext = applyns(oooL,psiL)
+@test check_mps_sanity(psiL_ext)
+
+@test siteinds(psiL_ext) == ss
+
+
+# Apply Right MPO to (equal length) Left tMPS: Chop 
+
+psiL = random_mps(ss, linkdims=20)
+
+psiL_chop = applyns(oooR,psiL)
+@test check_mps_sanity(psiL_chop)
+@test length(psiL_chop) == length(psiL)-n_ext
+
+# Apply Left tMPO to (equal length) Right tMPS: Extend
+psiR = random_mps(ss, linkdims=20)
+psiR_chop = applyn(oooL,psiR)
+@test check_mps_sanity(psiR_chop)
+@test length(psiR_chop) == length(psiR)-n_ext
