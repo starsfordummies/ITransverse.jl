3be81c70ee95663844d1826e0561051b173b77b9
Jan Schneider <jan.schneider@iff.csic.es>
2025-10-16 18:47:24 +0200

    upload progress

--- src/tmpo/tMPO-tMPS.jl
+++ src/tmpo/tMPO-tMPS.jl
@@ -41,46 +41,6 @@ function tMPS(N::Int; ortho_lims::UnitRange = 1:N)
     return tMPS(Vector{ITensor}(undef, N); ortho_lims = ortho_lims)
 end
 
-"""
-    tMPS([::Type{ElT} = Float64, ]sites; linkdims=1)
-
-Construct an MPS filled with Empty ITensors of type `ElT` from a collection of indices.
-
-Optionally specify the link dimension with the keyword argument `linkdims`, which by default is 1.
-
-In the future we may generalize `linkdims` to allow specifying each individual link dimension as a vector,
-and additionally allow specifying quantum numbers.
-"""
-function tMPS(
-        ::Type{T}, sites::Vector{<:Index}; linkdims::Union{Integer, Vector{<:Integer}} = 1
-    ) where {T <: Number}
-    _linkdims = _fill_linkdims(linkdims, sites)
-    N = length(sites)
-    v = Vector{ITensor}(undef, N)
-    if N == 1
-        v[1] = ITensor(T, sites[1])
-        return tMPS(v)
-    end
-
-    spaces = if hasqns(sites)
-        [[QN() => _linkdims[j]] for j in 1:(N - 1)]
-    else
-        [_linkdims[j] for j in 1:(N - 1)]
-    end
-
-    l = [Index(spaces[ii], "Link,l=$ii") for ii in 1:(N - 1)]
-    for ii in eachindex(sites)
-        s = sites[ii]
-        if ii == 1
-            v[ii] = ITensor(T, l[ii], s)
-        elseif ii == N
-            v[ii] = ITensor(T, dag(l[ii - 1]), s)
-        else
-            v[ii] = ITensor(T, dag(l[ii - 1]), s, l[ii])
-        end
-    end
-    return tMPS(v)
-end
 
 tMPS(sites::Vector{<:Index}, args...; kwargs...) = tMPS(Float64, sites, args...; kwargs...)
 
@@ -113,29 +73,6 @@ function convert(::Type{tMPO}, M::MPS)
     return tMPO(data(M); ortho_lims = ortho_lims(M))
 end
 
-function tMPO(::Type{ElT}, sites::Vector{<:Index}) where {ElT <: Number}
-    N = length(sites)
-    v = Vector{ITensor}(undef, N)
-    if N == 0
-        return tMPO()
-    elseif N == 1
-        v[1] = ITensor(ElT, dag(sites[1]), sites[1]')
-        return tMPO(v)
-    end
-    space_ii = all(hasqns, sites) ? [QN() => 1] : 1
-    l = [Index(space_ii, "Link,l=$ii") for ii in 1:(N - 1)]
-    for ii in eachindex(sites)
-        s = sites[ii]
-        if ii == 1
-            v[ii] = ITensor(ElT, dag(s), s', l[ii])
-        elseif ii == N
-            v[ii] = ITensor(ElT, dag(l[ii - 1]), dag(s), s')
-        else
-            v[ii] = ITensor(ElT, dag(l[ii - 1]), dag(s), s', l[ii])
-        end
-    end
-    return tMPO(v)
-end
 
 tMPO(sites::Vector{<:Index}) = tMPO(Float64, sites)
 
@@ -145,3 +82,40 @@ tMPO(sites::Vector{<:Index}) = tMPO(Float64, sites)
 Make an tMPO of length `N` filled with default ITensors.
 """
 tMPO(N::Int) = tMPO(Vector{ITensor}(undef, N))
+
+
+"""
+    construct_tMPS_tMPO(input::Matrix{ITensor})
+
+Construct two boundary tMPS and two tMPOs such for further use in the power method.
+Note that we assume the Matrix to have 4 rows and Nₜ+1 columns where Nₜ counts the time steps,
+and the first column describes the boundary (initial t=0) MPS in space. 
+"""
+function construct_tMPS_tMPO(input::Matrix{ITensor})
+  nrows, ncolumns = size(input)
+  @assert nrows == 4 "Assume input Matrix to have 4 rows!"
+
+  # MPS boundary defines the old physical site and the new link Index
+  # conceptually we rotate from old Site to new Link by +90°
+  has_MPS_boundary_at_end = if order(input[1,end]) == 2 
+    return true
+  elseif order(input[1,end]) == 3
+    return false
+  else
+    error("Order of boundary ITensor is $(order(input[1,end])), don't know what to do with it")
+  end
+
+  number_of_new_links = has_MPS_boundary_at_end ? ncolumns-1 : ncolumns
+
+  links_tMPS_L = [sim(only(inds(input[1,1],"Site")), tags="Link,time,nₜ=$(n)") for n in 0:number_of_new_links]
+  links_tMPO_L = [sim(only(inds(input[2,1],"Site")), tags="Link,time,nₜ=$(n)") for n in 0:number_of_new_links]
+  links_tMPO_R = [sim(only(inds(input[3,1],"Site")), tags="Link,time,nₜ=$(n)") for n in 0:number_of_new_links]
+  links_tMPS_R = [sim(only(inds(input[4,1],"Site")), tags="Link,time,nₜ=$(n)") for n in 0:number_of_new_links]
+
+  # the new sites are a bit special, the first (and perhaps last) site is (are) given by the boundary MPS
+  # while the bulk of the new sites are given by the old link of the time evolution operator
+  sites = [sim(only(inds(input[1,1],"Link")), tags="Site,time,nₜ=$(n)") for n in 0:number_of_new_links]
+
+
+end
+
