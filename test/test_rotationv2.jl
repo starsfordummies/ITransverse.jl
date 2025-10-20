c34b32953933f78b3384de50f00ac2d75d27f338
Jan Schneider <jan.schneider@iff.csic.es>
2025-10-20 20:39:28 +0200

    update format and comments

--- test/test_rotationv2.jl
+++ test/test_rotationv2.jl
@@ -1,6 +1,4 @@
 using ITensors, ITensorMPS, ITransverse
-using ITransverse: plus_state, up_state
-
 
 
 s = siteinds("S=1/2", 4)
@@ -18,35 +16,38 @@ N_t = 20
 
 function fill_bulk_evolution(nt, sites, dt, Jxx, λ, hz, gx)
   if iseven(nt)
-    return timeEvo_MPO_2ndOrder(sites, ["Id"], [λ], ["X"], [1.0], ["X"], [-Jxx], ["Z", "X"], [-hz, -gx], dt;)[:]
+    return timeEvo_MPO_2ndOrder(sites, ["Id"], [λ], ["X"], [1.0], ["X"], [-Jxx], ["Z", "X"], [-hz, -gx], dt;).data # return vector of ITensors rather than MPO
   else
-    return timeEvo_MPO_2ndOrder_LRflipped(sites, ["Id"], [λ], ["X"], [1.0], ["X"], [-Jxx], ["Z", "X"], [-hz, -gx], dt;)[:]
+    return timeEvo_MPO_2ndOrder_LRflipped(sites, ["Id"], [λ], ["X"], [1.0], ["X"], [-Jxx], ["Z", "X"], [-hz, -gx], dt;).data # return vector of ITensors rather than MPO
   end
 end
 
+# hcat takes the vector of vectors (which is the ouput of map(...)) and stacks them in a matrix column by column
 bulk_evo = hcat(
   map(
     nt -> fill_bulk_evolution(nt, s, dt, Jxx, λ, hz, gx),
-    range(1,N_t)
+    range(1, N_t)
   )...
 )
 
+# hcat the initial and final state (for the Loschmidt scenario)
 input = hcat(
   ψ0[:],
   bulk_evo,
   ψ0[:]
 )
 
+# construct the temporal MPS for L and R, and the corresponding transfer matrices TL and TR
 L, TL, TR, R = construct_unfolded_tMPS_tMPO(input)
 
 
-inner(L,R)
+inner(L, R)
 
 L2 = apply(TL, L)
 
 R2 = apply(TR, R)
 
-inner(L2,R2)
+inner(L2, R2)
 
 norm(L2)
 
