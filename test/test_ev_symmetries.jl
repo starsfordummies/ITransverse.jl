a9c114bfbbad87fd9e897cc123100787a043652a
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2025-10-29 19:01:51 +0100

    massage test

--- test/test_ev_symmetries.jl
+++ test/test_ev_symmetries.jl
@@ -4,7 +4,7 @@ using Test
 
 using ITransverse: up_state
 
-# Test computing exp. values using symmetries, folded and unfolded 
+@testset "Test computing exp. values using symmetries, folded and unfolded" begin 
 
 Lx = 20
 Nt = 12
@@ -18,28 +18,10 @@ Ut2 = ITransverse.ChainModels.build_expH_ising_murg_new(sites, mp, 0.1)
 
 Utvec, _ = ITransverse.ITenUtils.vectorize_mpo(Ut)
 Ut2vec, _ = ITransverse.ITenUtils.vectorize_mpo(Ut2)
-fidelity(Utvec, Ut2vec)
 
-norm.(Utvec) ≈ norm.(Ut2vec)
+@test fidelity(Utvec, Ut2vec) ≈ 1
 
-psi0 = MPS(sites, "Up")
-
-
-psiT = deepcopy(psi0)
-for jj = 1:Nt
-    psiT = apply(Ut2, psiT)
-end
-
-
-sites = siteinds("S=1/2", Lx; conserve_qns=false)
-Ut = build_expH_ising_murg(sites, mp, 0.1)
-Ut2 = ITransverse.ChainModels.build_expH_ising_murg_new(sites, mp, 0.1)
-
-Utvec, _ = ITransverse.ITenUtils.vectorize_mpo(Ut)
-Ut2vec, _ = ITransverse.ITenUtils.vectorize_mpo(Ut2)
-fidelity(Utvec, Ut2vec)
-
-norm.(Utvec) ≈ norm.(Ut2vec)
+@test norm.(Utvec) ≈ norm.(Ut2vec)
 
 psi0 = MPS(sites, "Up")
 
@@ -48,9 +30,14 @@ psiT = deepcopy(psi0)
 for jj = 1:Nt
     psiT = apply(Ut2, psiT)
 end
+
+
 
 expect(psiT, "Z")[div(length(psiT)+1,2)]
 
+
+
+## Now with QN conservation 
 
 ss = siteinds("S=1/2", Lx; conserve_szparity=true)
 
@@ -95,6 +82,9 @@ psi0 = MPS(ss, "Up")
 
 Ut_sym = ITransverse.ChainModels.build_expH_ising_murg_new(ss, mp, 0.1)
 UUt_sym = ITransverse.folded_UUt(Ut_sym)
+UUt_sym2 = ITransverse.folded_UUt(Ut_sym)
+
+
 
 rho0 = outer(psi0,dag(psi0)')
 rho0v, combis = ITransverse.ITenUtils.vectorize_mpo(rho0)
@@ -111,29 +101,20 @@ rho0v = replace_siteinds(rho0v, firstsiteinds(UUt_sym))
 vo_local_ops = replace_siteinds(vo_local_ops, siteinds(rho0v))
 
 
-inner(vo_local_ops, apply(UUt_sym, replace_siteinds(rho0v, firstsiteinds(UUt_sym))))  # works
+@test inner(vo_local_ops, apply(UUt_sym, replace_siteinds(rho0v, firstsiteinds(UUt_sym)))) ≈ 1 
 
 
-psiL, Tc, psiR = ITransverse.construct_tMPS_tMPO_2(rho0v, fill(UUt_sym, Nt), vo_local_ops)
+psiL, Tc, psiR = ITransverse.construct_tMPS_tMPO(rho0v, fill(UUt_sym, Nt), vo_local_ops)
 
 @test overlap_noconj(psiL, applyn(Tc, psiR)) ≈ 1
 
 
+# Now the local operator 
 local_ops_Z = [op("Z", s) for s in siteinds(psi0)]
-#local_ops[div(length(rhoT)+1,2)] = op("Z", siteind(psi0, div(length(psi0)+1,2)))
-
 o_local_ops_Z = MPO(local_ops_Z)
-
 vo_local_ops_Z, _ = ITransverse.ITenUtils.vectorize_mpo(o_local_ops_Z)
 
-
-
-_, Tc_Z, _ = ITransverse.construct_tMPS_tMPO_2(rho0v, fill(UUt_sym, Nt), vo_local_ops_Z)
-
-#construct_tMPS_tMPO(rho0v, [UUt_sym, UUt_sym], vo_local_ops) # errors
-
-#a,b,c,d = construct_tMPS_tMPO(hcat(rho0v.data, UUt_sym.data,UUt_sym.data,UUt_sym.data,UUt_sym.data, dag(vo_local_ops).data) )# errors
-#a,b,c,d = construct_tMPS_tMPO(hcat(rho0v.data, UUt_sym.data,UUt_sym.data,UUt_sym.data,UUt_sym.data, vo_local_ops.data) )# errors
+_, Tc_Z, _ = ITransverse.construct_tMPS_tMPO(rho0v, fill(UUt_sym, Nt), vo_local_ops_Z)
 
 lleft = psiL
 rright = psiR
@@ -149,3 +130,5 @@ lleft = replace_siteinds(lleft, firstsiteinds(Tc_Z))
 expval_LR(lleft, Tc_Z, rright)/overlap_noconj(lleft,rright)
 
 @test isapprox(expect(psiT, "Z")[div(length(psiT)+1,2)] , expval_LR(lleft, Tc_Z, rright)/overlap_noconj(lleft,rright); rtol=1e-5)
+
+end
