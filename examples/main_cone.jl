4a2822837cc20829410eca3c2a802c47f83064c9
Stefano Carignano <stefano.carignano@bsc.es>
2026-01-07 16:11:31 +0100

    m

--- examples/main_cone.jl
+++ examples/main_cone.jl
@@ -43,7 +43,21 @@ function main_cone()
     cone_params = ConeParams(;truncp, opt_method="RTM_R", optimize_op, 
     which_evs=["X","Z"], which_ents=["VN"], checkpoints=40:40:100)
 
-    psi, psiR, chis, expvals, entropies, infos, last_cp = run_cone(c0, b, cone_params, Nsteps)
+    cp = DoCheckpoint(
+        "cp_cone.jld2";
+        params=tp,
+        save_at=5,
+        observables = (
+            SVN = s -> vn_entanglement_entropy(s.R),
+            overlap = s -> overlap_noconj(s.L, s.R),
+            ZX = s -> compute_expvals(s.L, s.R, ["Z","X"], s.b)
+        ),
+        latest_savers = (
+            L = s -> s.L,
+            R = s -> s.R,
+            b = s -> s.b
+        )
+    )
 
     return  psi, psiR, chis, expvals, entropies, infos, last_cp
 
