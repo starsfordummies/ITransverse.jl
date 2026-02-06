c17985aa4f7322b5dc62fe0eb209243e78b534f7
Stefano Carignano <stefano.carignano@bsc.es>
2026-02-06 17:55:16 +0100

    modernize sweeps

--- src/truncation_sweeps/sweeps_sym.jl
+++ src/truncation_sweeps/sweeps_sym.jl
@@ -5,30 +5,33 @@ followed by a Left >>> sweep with truncation on the generalized SVs
 returns psi_ortho (in generalized symmetric *left* canonical form) normalized to (psi|psi)=1
 and ents_sites = log(sum(S_i)) for each site
 """
-function truncate_lsweep_sym(in_psi::MPS; cutoff::Float64, chi_max::Int, method::String)
+function truncate_lsweep_sym(in_psi::MPS; cutoff::Float64, maxdim::Int, method::String)
 
-    elt = eltype(in_psi[1])
     mpslen = length(in_psi)
 
     psi_ortho = orthogonalize(in_psi,1)
     sits = siteinds(psi_ortho)
+    sits_prime = prime(sits)
 
     ents_sites = ComplexF64[] 
 
     XUinv= ITensors.OneITensor()
     left_env = ITensors.OneITensor()
 
+    S_all = zeros(Float64, maxdim, mpslen-1)
+
     for ii = 1:mpslen-1
 
         Ai = XUinv * psi_ortho[ii]
 
         left_env *= Ai
-        left_env *= Ai'
-        left_env *= delta(elt, sits[ii],sits[ii]')
+        left_env *= replaceind(Ai', sits_prime[ii] => sits[ii])
+
+        #left_env *= delta(elt, sits[ii],sits[ii]')
 
 
         if method == "SVD"
-            F = symm_svd(left_env, ind(left_env,1), cutoff=cutoff, maxdim=chi_max)
+            F = symm_svd(left_env, ind(left_env,1); cutoff, maxdim)
             U = F.U
             S = F.S
 
@@ -41,7 +44,7 @@ function truncate_lsweep_sym(in_psi::MPS; cutoff::Float64, chi_max::Int, method:
         # TODO this likely doesn't work on GPU ..
         # maybe add a NDTensors.cpu()
         elseif method == "EIG"
-            F = symm_oeig(left_env, ind(left_env,1); cutoff)
+            F = symm_oeig(left_env, ind(left_env,1); cutoff, maxdim)
             U = F.V
             S = F.D
 
@@ -59,8 +62,12 @@ function truncate_lsweep_sym(in_psi::MPS; cutoff::Float64, chi_max::Int, method:
         left_env *= XU'
 
         # If we build "SVD" generalized entropy, normalize them to one 
-        S = NDTensors.cpu(S./sum(S))
-        push!(ents_sites, scalar(-S*log.(S)))
+        # S = NDTensors.cpu(S./sum(S))
+        # push!(ents_sites, scalar(-S*log.(S)))
+
+        Svec = collect(S.tensor.storage.data)/sum(F.S)  
+ 
+        S_all[1:length(Svec), ii-1] .= Svec  
     end
 
     An = XUinv * psi_ortho[end]
@@ -69,25 +76,22 @@ function truncate_lsweep_sym(in_psi::MPS; cutoff::Float64, chi_max::Int, method:
 
     psi_ortho[end] = An
 
-    if abs(scalar(overlap)) > 1e10 || abs(scalar(overlap)) < 1e-10
-        @warn ("Careful! overlap overflowing? = $(scalar(overlap))")
-    end
-
     @debug "Sweep done, normalization $(overlap_noconj(psi_ortho, psi_ortho))"
 
-    return psi_ortho, ents_sites, overlap
+    return psi_ortho, S_all, overlap
 
 end
 
 
 
 
-""" Bring the MPS to symmetric right generalized canonical form """
+""" Symmetric truncate for MPS optimizing RTM |psi^*><psi| """
 function truncate_rsweep_sym(in_psi::MPS; cutoff::Float64, chi_max::Int, method::String, fast::Bool=false)
 
     mpslen = length(in_psi)
     elt = eltype(in_psi[1])
     sits = siteinds(in_psi)
+    sits_prime = prime(sits)
 
     # first bring to LEFT standard canonical form 
     psi_ortho = orthogonalize(in_psi, mpslen)
@@ -95,20 +99,21 @@ function truncate_rsweep_sym(in_psi::MPS; cutoff::Float64, chi_max::Int, method:
     XUinv= ITensors.OneITensor()
     right_env = ITensors.OneITensor()
 
-    ents_sites = fill(zero(ComplexF64), mpslen-1)  # Float64[]
+    # ents_sites = fill(0., mpslen-1)  # Float64[]
+
+    S_all = zeros(Float64, chi_max, mpslen-1)
 
     for ii = mpslen:-1:2
         Ai = XUinv * psi_ortho[ii]
 
         right_env *= Ai
-        
-        right_env *= Ai'
-        right_env *= delta(elt, sits[ii], sits[ii]')
-        
+        #right_env *= Ai'
+        #right_env *= delta(elt, sits[ii], sits_prime[ii])
+        right_env *= replaceind(Ai', sits_prime[ii] => sits[ii])
+
         # Alt: needs to have specified link!
         #right_env *= prime(Ai, "Link")
 
-        #@info inds(right_env)
         @assert order(right_env) == 2
 
         if method == "SVD"
@@ -119,7 +124,7 @@ function truncate_rsweep_sym(in_psi::MPS; cutoff::Float64, chi_max::Int, method:
             XU = dag(U)
             XUinv = U
 
-            if fast 
+            if fast # if we don't care about putting the output psi in generalized canonical form
                 right_env /= sum(S)
             else
                 sqS = S.^(0.5)
@@ -154,8 +159,12 @@ function truncate_rsweep_sym(in_psi::MPS; cutoff::Float64, chi_max::Int, method:
 
 
         # If we build "SVD" generalized entropy, normalize SV to one 
-        S = S/sum(S)
-        ents_sites[ii-1] =  scalar(-S*log.(S))
+        #S = S/sum(S)
+        #ents_sites[ii-1] =  scalar(-S*log.(S))
+
+        Svec = collect(S.tensor.storage.data)/sum(F.S)  
+ 
+        S_all[1:length(Svec), ii-1] .= Svec  
     end
 
     # the last one 
@@ -166,8 +175,7 @@ function truncate_rsweep_sym(in_psi::MPS; cutoff::Float64, chi_max::Int, method:
     # normalize overlap to 1 at the last tensor ?
     psi_ortho[1] =  An # /sqrt(scalar(overlap))
 
-
-    return psi_ortho, ents_sites, overlap
+    return psi_ortho, S_all, overlap
 
 end
 
@@ -179,9 +187,9 @@ end
 Bring the MPS to generalized *left* canonical form without truncating (as far as possible)
 This is symmetric, so we use symmetric eigenvalue decomposition, A => O D O^T with O complex orthogonal 
 """
-function gen_canonical_left(in_mps::MPS)
+function gen_canonical_left(in_mps::MPS)  # TODO: polar decomp?
     temp = deepcopy(in_mps)
-    psi_leftgencan, _, _ = truncate_lsweep_sym(temp; cutoff=1e-15, chi_max=2*maxlinkdim(in_mps), method="EIG")
+    psi_leftgencan, _, _ = truncate_lsweep_sym(temp; cutoff=1e-14, maxdim=2*maxlinkdim(in_mps), method="EIG")
     return psi_leftgencan
 end
 
@@ -190,6 +198,13 @@ Just bring the MPS to generalized *right* canonical form without truncating (as 
 TODO should use chi_min here to make sure ! 
 """
 function gen_canonical_right(in_mps::MPS)
-    psi_rightgencan, _, _ = truncate_rsweep_sym(in_mps; cutoff=1e-15, chi_max=2*maxlinkdim(in_mps), method="EIG")
+    psi_rightgencan, _, _ = truncate_rsweep_sym(in_mps; cutoff=1e-14, maxdim=2*maxlinkdim(in_mps), method="EIG")
     return psi_rightgencan
+end
+
+
+
+function tcontract(::Algorithm"RTMsym", A::MPO, ψ::MPS; preserve_tags_mps::Bool=false, kwargs...)
+    psi, _ = apply(A, ψ, alg="naive", preserve_tags_mps, truncate=false)
+    psi = truncate_rsweep_sym(psi, kwargs...)
 end
