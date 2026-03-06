2f21f3a925ce79fcf372c29cb74a1643bfe69678
Stefano Carignano <stefano.carignano@bsc.es>
2026-03-06 17:54:45 +0100

    gensym ents segment

--- src/entropies/gen_sym_entropies.jl
+++ src/entropies/gen_sym_entropies.jl
@@ -239,6 +239,27 @@ function gensym_renyi_entropies(psiL::MPS; which_ents=[0.5,1,2], bring_left_gen:
 end
 
 
+
+""" Computes generalized entropies for a segment by diagonalizing the RTM - expensive!  (chi^4) """
+function gensym_renyi_entropies_segment(psi::MPS, iA::Int, fA::Int; which_ents=[0.5,1,2], normalize_eigs::Bool=true)
+
+    psig = ITransverse.gen_canonical(psi, iA+1)
+
+    psigp = prime(linkinds, psig)
+    rhoc = ITensor(1)
+    for kk = iA:fA
+        rhoc *= psig[kk]
+        rhoc *= psigp[kk]
+    end
+
+    F = ITransverse.ITenUtils.symm_oeig(rhoc, (linkind(psig,iA-1), linkind(psig,fA)); cutoff=1e-13)
+    eigvals_tau = normalize_eigs ? F.D/sum(F.D) : F.D
+
+
+    renyi_entropies(eigvals_tau.tensor.storage.data; which_ents)
+end
+
+
 """ Computes the generalized SVD entropies: Given input MPS |phi> and <psi|, < diagonalizes the RTM |phi><psi| 
 and builds alpha-order Renyi entropies as specified by `which_ents` (alpha=1 ie. VN is always computed). Returns a dict """
 function gensvd_renyi_entropies(psi::MPS, phi::MPS; which_ents=[0.5,1,2], normalize_eigs::Bool=true)
@@ -348,6 +369,7 @@ end
 function gen_renyi2_sym_interval_manual(psi::MPS, iA::Int, fA::Int)
 
     LL = length(psi)
+    normalization = overlap_noconj(psi,psi)
 
     psip = prime(linkinds, psi)
     lenv = ITensor(1)
@@ -374,7 +396,7 @@ function gen_renyi2_sym_interval_manual(psi::MPS, iA::Int, fA::Int)
     t2 *= renv
     t2 *= renv''
     
-    t2 = scalar(t2)
+    t2 = scalar(t2)/normalization^2
 
     return -log(t2)
 end
