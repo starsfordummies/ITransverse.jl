f2b268b7b881a9f2194978152ca31a1efa8d235e
Stefano Carignano <stefano.carignano@bsc.es>
2026-03-24 13:55:31 +0100

    test

--- test/test_tlrcontract.jl
+++ test/test_tlrcontract.jl
@@ -1,13 +1,13 @@
 using ITensors, ITensorMPS, ITransverse
 using Test
 
-ss = siteinds("S=1/2", 20)
-ss2 = siteinds("S=1/2", 24)
+ss = siteinds("S=1/2", 40)
+ss2 = siteinds("S=1/2", 47)
 ss2[1:length(ss)] = ss
 ss
 ss2
-ψL = random_mps(ComplexF64, ss, linkdims=32) 
-ψR = random_mps(ComplexF64, ss, linkdims=50) 
+ψL = random_mps(ComplexF64, ss, linkdims=128) 
+ψR = random_mps(ComplexF64, ss, linkdims=100) 
 
 AL = random_mpo(ss2) + im*random_mpo(ss2)
 AR = random_mpo(ss2) + im*random_mpo(ss2)
@@ -17,11 +17,43 @@ for kk = length(ss)+1:length(ss2)
     AL[kk] *= ITensor([1,0],ss2[kk]')
 end
 
-LO = applyns(AL, ψL; cutoff=1e-12, maxdim=200)
-OR = applyn(AR, ψR; cutoff=1e-12, maxdim=200)
+LO = applyns(AL, ψL; truncate=false)
+OR = applyn(AR, ψR; truncate=false)
 
-llt, rrt, sst = ITransverse.truncate_sweep(LO, OR; cutoff=1e-10, maxdim=40, direction="right")
+cutoff = 1e-10
+maxdim=120
 
-ll, rr, ss = ITransverse.tlrcontract(ψL, AL, AR, ψR; cutoff=1e-10, maxdim=40)
- abs( gen_fidelity(llt,rrt) - gen_fidelity(ll,rr))
-@test abs( gen_fidelity(llt,rrt) - gen_fidelity(ll,rr)) < 1e-5
+llt, rrt, sst = ITransverse.truncate_sweep(LO, OR; cutoff, maxdim, direction="right")
+llt2, rrt2, sst2 = ITransverse.truncate_rsweep_new(LO, OR; cutoff, maxdim)
+
+ll, rr, ss = ITransverse.tlrcontract_old(ψL, AL, AR, ψR; cutoff, maxdim)
+ abs( gen_fidelity(llt,rrt) - gen_fidelity(ll,rr))/abs(gen_fidelity(llt,rrt))
+#@test abs( gen_fidelity(llt,rrt) - gen_fidelity(ll,rr)) < 1e-5
+
+llc, rrc, ssc = ITransverse.tlrcontract(ψL, AL, AR, ψR; cutoff, maxdim)
+llc, rrc, ssc = ITransverse.tlrapply(ψL, AL, AR, ψR; cutoff, maxdim)
+
+fidelity(ll, llc)
+fidelity(ll, llt)
+fidelity(llt, llt2)
+fidelity(llc, llt2)
+fidelity(rrc, rrt2)
+
+fidelity(llc, llt)
+fidelity(rrc, rrt)
+
+fidelity(rr, rrc)
+fidelity(rr, llt)
+
+maxlinkdim(ll)
+maxlinkdim(llt)
+maxlinkdim(llt2)
+maxlinkdim(llc)
+
+llc
+rrc
+
+
+@btime ITransverse.tlrcontract(ψL, AL, AR, ψR; cutoff, maxdim);
+
+@btime ITransverse.tlrcontract_cone(ψL, AL, AR, ψR; cutoff, maxdim);
