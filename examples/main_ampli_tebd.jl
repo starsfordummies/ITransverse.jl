990242f2ce925bf32d73e8f30623a9720612c31f
Stefano Carignano <stefano.carignano@bsc.es>
2026-04-26 14:54:03 +0200

    upd to new abi

--- examples/main_ampli_tebd.jl
+++ examples/main_ampli_tebd.jl
@@ -7,13 +7,13 @@ psi0 = MPS(ss, "Up")
 mp = IsingParams(1, 0.5, -1.05)
 tp = ising_tp(integrable=false)
 tp = tMPOParams(tp; mp=mp)
-psi_full =  ITransverse.tebd(psi0, tp, 100, (cutoff=1e-12, maxdim=1024))
+psi_full =  ITransverse.tebd(30, psi0, tp; cutoff=1e-12, maxdim=1024)
 
 amply = inner(psi_full, psi0)
 
 amplis_chi = []
 for chi = 10:10:120
-    psi_t = ITransverse.tebd(psi0, tp, 100, (cutoff=1e-12, maxdim=chi))
+    psi_t = ITransverse.tebd(30, psi0, tp; cutoff=1e-12, maxdim=chi)
     ampli = inner(psi_t, psi0)
     push!(amplis_chi, ampli)
 end
